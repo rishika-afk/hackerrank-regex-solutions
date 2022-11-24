ab89ca599bd384811219ace6679ab77fa68cc596
Rishika Gupta <73276425+rishika-afk@users.noreply.github.com>
2022-11-24 13:33:22 +0530

    Create Matching Anything But a New Line.cpp

---
+++ Matching Anything But a New Line.cpp
@@ -0,0 +1,18 @@
+//cpp solution
+
+#include <cmath>
+#include <cstdio>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+#include <regex>
+using namespace std;
+
+
+int main() {
+    regex strange(R"(.{3}\..{3}\..{3}\..{3})");
+    string input_str;
+    getline(cin, input_str);
+    cout << boolalpha << regex_match(input_str, strange) << endl;
+    return 0;
+}
